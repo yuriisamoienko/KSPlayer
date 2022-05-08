d23f415743e3c97a5fbed45b0f15978fa982babe
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-05-08 18:50:48 +0300

    added xcodeproj with shared scheme

---
+++ KSPlayerFramework/KSPlayerFramework.h
@@ -0,0 +1,18 @@
+//
+//  KSPlayerFramework.h
+//  KSPlayerFramework
+//
+//  Created by Yurii Samoienko on 08.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSPlayerFramework.
+FOUNDATION_EXPORT double KSPlayerFrameworkVersionNumber;
+
+//! Project version string for KSPlayerFramework.
+FOUNDATION_EXPORT const unsigned char KSPlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSPlayerFramework/PublicHeader.h>
+
+
